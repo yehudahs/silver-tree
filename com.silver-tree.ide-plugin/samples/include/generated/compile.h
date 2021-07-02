79540be7bba37889b1bfc663cf01a64b7b03cd1f
yehudas <yehuda.s@neuroblade.ai>
2021-07-02 09:12:58 +0300

    mv all dts files into arch folder and add include folder with .h files for the preprocessor

---
+++ com.silver-tree.ide-plugin/samples/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#3 SMP Wed Jun 30 15:32:03 IDT 2021"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "guest-Standard-PC-i440FX-PIIX-1996"
+#define LINUX_COMPILER "gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0, GNU ld (GNU Binutils for Ubuntu) 2.34"
